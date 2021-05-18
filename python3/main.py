cf94df573bcea898d556fa5fffb92e017c333a7b
bscobe4 <78442276+bscobe4@users.noreply.github.com>
2021-05-18 16:42:45 -0700

    Update main.py

--- python3/main.py
+++ python3/main.py
@@ -29,26 +29,35 @@ try:
     #        print ("\33[9A")
     row = 0  # To indicate if header needs to be written
     numRows = 10
+    isLogging = True
+    fastLogging = True
+    timeStart = time.time()
 
     with open('output/pressureFlowData.csv', 'w', newline='') as csvfile:
         dataWriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         if row == 0:
             dataWriter.writerow(
-                ['Rod 1'] + ['Back 1'] + ['Rod 2'] + ['Back 2'] + ['Rod 3'] + ['Back 3'] + ['Flow 1'] + [
-                    'Flow 2'])  # write header
+                ['Rod 1 (V)'] + ['Back 1 (V)'] + ['Rod 2 (V)'] + ['Back 2 (V)'] + ['Rod 3 (V)'] + ['Back 3 (V)'] + ['Flow 1 (V)'] + [
+                    'Flow 2'] + ['Time (s)'])  # write header
             row += 1  # toggle to show that header has been written
-        while (row <= numRows): #DEBUG replace with GPIO trigger
+        #while (row <= numRows): #DEBUG replace with GPIO trigger
+        while isLogging:
             ADC_Value = ADC.ADS1256_GetAll()
+            timeDelta = time.time() - timeStart
             
             dataWriter.writerow(
                 ['%lf' % (ADC_Value[0] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[1] * 5.0 / 0x7fffff)] + [
                     '%lf' % (ADC_Value[2] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[3] * 5.0 / 0x7fffff)] + [
                     '%lf' % (ADC_Value[4] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[5] * 5.0 / 0x7fffff)] + [
-                    '%lf' % (ADC_Value[6] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[7] * 5.0 / 0x7fffff)])
-            
+                    '%lf' % (ADC_Value[6] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[7] * 5.0 / 0x7fffff)] + ['%lf' % timeDelta])
+            #print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
             row += 1
             csvfile.flush()
-            time.sleep(1)
+            
+            if not fastLogging:
+              time.sleep(1)  #1 Hz
+            else:
+              time.sleep(0.001)  #1kHz *DEBUG: this implementation only leads to about 20 Hz real-time
 
 except:
     GPIO.cleanup()

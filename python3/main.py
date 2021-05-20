f972e15f86294c933e185e85882fc6fdb2611c38
bscobe4 <78442276+bscobe4@users.noreply.github.com>
2021-05-20 02:25:35 -0700

    New keypress and fastlogging subprocess work again

--- python3/main.py
+++ python3/main.py
@@ -2,45 +2,65 @@
 # -*- coding:utf-8 -*-
 
 import os
+import subprocess
 import time
 import csv  # to write to csv file for nfs
 import ADS1256
 import RPi.GPIO as GPIO
+import keyboard
+import sys
+import select
+import tty
+import termios
 
 workpath = '/home/pi/Documents/GMT_PressFlowMonitor/python3/'
+slowFile = 'output/slowPFMdata.csv'
+fastFile = 'output/fastPFMdata.csv'
+
+KEY_ESC = '\x1b'
+KEY_FAST = 'f'
+
+row = 0  # To indicate if header needs to be written
+numRows = 10
+isLogging = True
+startFast = False
+
+keyListen_fastLog = True
+escape = False
+
+#**Function Definitions**
+def key_press(key):
+  print(key.name)
+  if key.name == "esc":
+    escape = True
+  if key.name == "ctrl+f":
+    startFast = True
+
+def isData(): #keypress-function to know if there is input available
+  return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []) #keypress
 
 os.chdir(os.path.dirname(workpath))
 
+old_settings = termios.tcgetattr(sys.stdin) #keypress-save terminal settings
+
 try:
+    tty.setcbreak(sys.stdin.fileno()) #keypress- set the terminal to character input mode
     ADC = ADS1256.ADS1256()
     ADC.ADS1256_init()
 
-
-    #    while(1):
-    #        ADC_Value = ADC.ADS1256_GetAll()
-    #        print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff)) #cylinder 1 back
-    #        print ("1 ADC = %lf"%(ADC_Value[1]*5.0/0x7fffff)) #cylinder 1 rod
-    #        print ("2 ADC = %lf"%(ADC_Value[2]*5.0/0x7fffff)) #cylinder 2 back
-    #        print ("3 ADC = %lf"%(ADC_Value[3]*5.0/0x7fffff)) #cylinder 2 rod
-    #        print ("4 ADC = %lf"%(ADC_Value[4]*5.0/0x7fffff)) #cylinder 3 back
-    #        print ("5 ADC = %lf"%(ADC_Value[5]*5.0/0x7fffff)) #cylinder 3 rod
-    #        print ("6 ADC = %lf"%(ADC_Value[6]*5.0/0x7fffff)) #flow sensor 1
-    #        print ("7 ADC = %lf"%(ADC_Value[7]*5.0/0x7fffff)) #flow sensor 2 (if used)
-    #        print ("\33[9A")
-    row = 0  # To indicate if header needs to be written
-    numRows = 10
-    isLogging = True
-    fastLogging = True
     timeStart = time.time()
-
-    with open('output/pressureFlowData.csv', 'w', newline='') as csvfile:
+    #print("debug1")
+    with open(slowFile, 'w', newline='') as csvfile:
         dataWriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        #print("debug2")
+        
         if row == 0:
             dataWriter.writerow(
                 ['Rod 1 (V)'] + ['Back 1 (V)'] + ['Rod 2 (V)'] + ['Back 2 (V)'] + ['Rod 3 (V)'] + ['Back 3 (V)'] + ['Flow 1 (V)'] + [
                     'Flow 2'] + ['Time (s)'])  # write header
             row += 1  # toggle to show that header has been written
         #while (row <= numRows): #DEBUG replace with GPIO trigger
+        #print("debug3")
         while isLogging:
             ADC_Value = ADC.ADS1256_GetAll()
             timeDelta = time.time() - timeStart
@@ -54,12 +74,35 @@ try:
             row += 1
             csvfile.flush()
             
-            if not fastLogging:
+            #keyboard.on_press(key_press)
+            
+            #keypress- listen for input
+            if isData():
+              c = sys.stdin.read(1)
+              if c == KEY_ESC: #esc key
+                escape = True
+              if c == KEY_FAST: #f key
+                startFast = True
+            
+            if escape:
+              isLogging = False
+
+            if not startFast:
               time.sleep(1)  #1 Hz
             else:
-              time.sleep(0.001)  #1kHz *DEBUG: this implementation only leads to about 20 Hz real-time
+              print("started fast logging")
+              fastLogProcess = subprocess.Popen(["python3", "fastLogging.py"]) #can't interact with this subprocess
+              startFast = False
+              time.sleep(1)
+              
+              #time.sleep(0.001)  #1kHz *DEBUG: this implementation only leads to about 20 Hz real-time
 
 except:
     GPIO.cleanup()
-    print("\r\nProgram end     ")
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings) #keypress-restore console state
+    print("\r\nexited pressure & flow monitor (exception)")
     exit()
+    
+finally:
+      termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings) #keypress-restore console state
+      print("exited pressure & flow monitor (exited)")

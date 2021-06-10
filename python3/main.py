a9c13d2a771b3ed305c974c5652bd0bede0c55d0
bscobe4 <78442276+bscobe4@users.noreply.github.com>
2021-06-10 11:24:29 -0700

    Update main.py

--- python3/main.py
+++ python3/main.py
@@ -30,9 +30,11 @@ fastrow = 0
 fastLoggingTime = 10
 isLogging = True
 startFast = False
+isFast = False
 
 keyListen_fastLog = True
 escape = False
+timeLast = 2
 
 #**Function Definitions**
 
@@ -49,7 +51,7 @@ try:
     config = configparser.ConfigParser()
     config.read('PFMconfig.ini')
     outputpath = config['DEFAULT']['outputAddress']#workpath #debug- need to read from config file later
-    slowFile = outputpath + slowFile
+    #slowFile = outputpath + slowFile
     
     tty.setcbreak(sys.stdin.fileno()) #keypress- set the terminal to character input mode
     
@@ -59,14 +61,14 @@ try:
     timeStart = time.time()
     dNow = datetime.now()
     strDate = dNow.strftime("%m-%d-%Y_%H:%M:%S")
-    slowFile = outputpath + fastFile + strDate + '.csv'
+    slowFile = outputpath + slowFile + strDate + '.csv'
     
     
     
-    with open(slowFile, 'w', newline='') as slowCSV, open (fastFile, 'w', newline='') as fastCSV:
+    with open(slowFile, 'w', newline='') as slowCSV:
     
         slowWriter = csv.writer(slowCSV, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        fastWriter = csv.writer(fastCSV, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        #fastWriter = csv.writer(fastCSV, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         
         if row == 0:
             slowWriter.writerow(
@@ -74,38 +76,93 @@ try:
                     'Flow 2 (V)'] + ['Time (s)'])  # write header
             row += 1  # toggle to show that header has been written
         #while (row <= numRows): #DEBUG replace with GPIO trigger
+        
         while isLogging:
             ADC_Value = ADC.ADS1256_GetAll()
             timeDelta = time.time() - timeStart
+            print('Delta')
             
-            slowWriter.writerow(
-                ['%lf' % (ADC_Value[0] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[1] * 5.0 / 0x7fffff)] + [
-                    '%lf' % (ADC_Value[2] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[3] * 5.0 / 0x7fffff)] + [
-                    '%lf' % (ADC_Value[4] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[5] * 5.0 / 0x7fffff)] + [
-                    '%lf' % (ADC_Value[6] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[7] * 5.0 / 0x7fffff)] + ['%lf' % timeDelta])
-            #print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
-            row += 1
-            slowCSV.flush()
+            if startFast:
+              dNow = datetime.now()
+              strDate = dNow.strftime("%m-%d-%Y_%H:%M:%S")
+              fastFile = outputpath + fastFile + strDate + '.csv'
+              fastLogDuration = float(config['DEFAULT']['fastLogDuration'])
+              timeStartFast = timeDelta
+              print("debug 1")
+              
             
+            if timeDelta-timeLast > 1:
+              slowWriter.writerow(
+                  ['%lf' % (ADC_Value[0] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[1] * 5.0 / 0x7fffff)] + [
+                      '%lf' % (ADC_Value[2] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[3] * 5.0 / 0x7fffff)] + [
+                      '%lf' % (ADC_Value[4] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[5] * 5.0 / 0x7fffff)] + [
+                      '%lf' % (ADC_Value[6] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[7] * 5.0 / 0x7fffff)] + ['%lf' % timeDelta])
+              #print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
+              row += 1
+              slowCSV.flush()
+            
+            timeLast = timeDelta
+            print('timeLast')
+            
+            if isFast:
+            
+              with open(fastFile, 'w', newline='') as fastCSV:
+              
+                if startFast:
+                  print('debug 2')
+                  fastRow = 0
+                  fastWriter = csv.writer(fastCSV, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                  startFast = False
+                  fastWriter.writerow(
+                    ['Rod 1 (V)'] + ['Back 1 (V)'] + ['Rod 2 (V)'] + ['Back 2 (V)'] + ['Rod 3 (V)'] + ['Back 3 (V)'] + ['Flow 1 (V)'] + [
+                    'Flow 2 (V)'] + ['Time (s)'])  # write header
+                  fastrow += 1  # toggle to show that header has been written
+                  print('debug 3')
+                  
+                print('debug 4')
+
+              
+                #fastWriter.writerow(
+                #  ['%lf' % (ADC_Value[0] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[1] * 5.0 / 0x7fffff)] + [
+                #    '%lf' % (ADC_Value[2] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[3] * 5.0 / 0x7fffff)] + [
+                #    '%lf' % (ADC_Value[4] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[5] * 5.0 / 0x7fffff)] + [
+                #    '%lf' % (ADC_Value[6] * 5.0 / 0x7fffff)] + ['%lf' % (ADC_Value[7] * 5.0 / 0x7fffff)] + ['%lf' % timeDelta])
+                fastrow += 1
+                fastCSV.flush()
+
+            if isFast:
+              if timeDelta - timeStartFast > fastLogDuration:
+                escape = True 
+                        
             #keypress- listen for input
             if isData():
               c = sys.stdin.read(1)
               if c == KEY_ESC: #esc key
                 escape = True
               if c == KEY_FAST: #f key
-                startFast = True              
+                if not isFast:
+                  startFast = True
+                  isFast = True
+                  print("started fast logging") 
+                else:
+                  print("Fast Logging already running. Press ESC to abort")   
             
             if escape:
-              isLogging = False
+              if not isFast:
+                isLogging = False
+              else:
+                isFast = False
+                escape = False
+                fastFile = 'output/fastPFMdata'
+                print("\r\nexited Fast Logging")
 
-            if not startFast:
+            if not isFast:
               time.sleep(1)  #1 Hz
             else:
-              print("started fast logging")
+
               #fastLogProcess = subprocess.Popen(["python3", "fastLogging.py"]) #can't interact with this subprocess
-              startFast = False
+              #startFast = False
               #time.sleep(1)
-              
               time.sleep(0.001)  #1kHz *DEBUG: this implementation only leads to about 20 Hz real-time
 
 except:
